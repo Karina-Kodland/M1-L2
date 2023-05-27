cbb0351c586879b1dfe13c8a26dd520c8936d7f1
Karina-Kodland <129550143+Karina-Kodland@users.noreply.github.com>
2023-05-27 11:05:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='-', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -18,4 +18,9 @@ async def hello(ctx):
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
-bot.run("ТУТ СЕКРЕТНЫЙ ТОКЕН")
+@bot.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+bot.run('')
